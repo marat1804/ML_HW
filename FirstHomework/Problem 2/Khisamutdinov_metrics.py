3b84814a48c5e4048e969795fdb9f15f08bb8624
Marat Khisamutdinov <maratcoop11@gmail.com>
2021-03-31 16:23:51 +0300

    Fixed metrics

--- FirstHomework/Problem 2/Khisamutdinov_metrics.py
+++ FirstHomework/Problem 2/Khisamutdinov_metrics.py
@@ -7,18 +7,24 @@ import numpy as np
 
 
 def choose_class(y_p, y_true, percent):
-    y_p_1 = y_p[:, -1]
-    quantile = np.percentile(a=y_p_1, q=100-percent)
-    top_elements = [(y_p_1[i], i) for i in range(len(y_p_1)) if y_p_1[i] >= quantile]
-    index = [i[1] for i in top_elements]
+    if percent:
+        y_p_1 = y_p[:, -1]
+        quantile = np.percentile(a=y_p_1, q=100-percent)
+        top_elements = [(y_p_1[i], i) for i in range(len(y_p_1)) if y_p_1[i] >= quantile]
+        index = [i[1] for i in top_elements]
+        top_elements = [i[0] for i in top_elements]
+        threshold = (100 - percent) / 100
+        top_y = [y_true[j] for j in index]
+    else:
+        top_y = y_true
+        threshold = 0.5
+        top_elements = y_p[:, -1]
     top_data = []
-    threshold = (100 - percent) / 100
     for element in top_elements:
-        if element[0] > threshold:
+        if element > threshold:
             top_data.append(1)
         else:
             top_data.append(0)
-    top_y = [y_true[j] for j in index]
     tp, fp, tn, fn = divide(top_y, top_data)
     return tp, fp, tn, fn
 
@@ -37,27 +43,27 @@ def divide(y_t, y_p):
     return tp, fp, tn, fn
 
 
-def accuracy_score(y_true, y_pred, percent=50):
+def accuracy_score(y_true, y_pred, percent=None):
     tp, fp, tn, fn = choose_class(y_pred, y_true,  percent)
     return (tp + tn)/(tp + tn + fp + fn)
 
 
-def precision_score(y_true, y_pred, percent=50):
+def precision_score(y_true, y_pred, percent=None):
     tp, fp, tn, fn = choose_class(y_pred, y_true,  percent)
     return tp / (tp + fp)
 
 
-def recall_score(y_true, y_pred, percent=50):
+def recall_score(y_true, y_pred, percent=None):
     tp, fp, tn, fn = choose_class(y_pred, y_true,  percent)
     return tp / (tp + fn)
 
 
-def lift_score(y_true, y_pred, percent=50):
+def lift_score(y_true, y_pred, percent=None):
     tp, fp, tn, fn = choose_class(y_pred, y_true,  percent)
     return (tp / (tp + fp)) / ((tp + fn) / (tp + tn + fp + fn))
 
 
-def f1_score(y_true, y_pred, percent=50):
+def f1_score(y_true, y_pred, percent=None):
     tp, fp, tn, fn = choose_class(y_pred, y_true,  percent)
     recall = tp / (tp + fn)
     precision = tp / (tp + fp)
